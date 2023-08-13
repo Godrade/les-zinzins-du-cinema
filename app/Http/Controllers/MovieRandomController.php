f6954818352bb942b15c6ea7abe94f65edcb1953
Godrade <jorisr@hotmail.fr>
2023-08-13 20:12:20 +0200

    Correction de l'erreur 500 si aucun film trouvé pendant la recherche aléatoire

--- app/Http/Controllers/MovieRandomController.php
+++ app/Http/Controllers/MovieRandomController.php
@@ -9,6 +9,10 @@ class MovieRandomController extends Controller
     public function index(Listing $listing)
     {
         $movie = $listing->movies()->inRandomOrder()->first();
+        if (!$movie) {
+            return redirect()->back()->withErrors("Aucun film n'a été trouvé");
+        }
+
         return redirect()->route('search.show', $movie->tmdb_id);
     }
 }
